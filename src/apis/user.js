68545370f75a3ad94089a6128bea5df36a4b8c02
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-08-16 03:26:32 +0530

    Update pipeplus subscription method

--- src/apis/user.js
+++ src/apis/user.js
@@ -5,7 +5,7 @@ export const user = {
     subscriptions: async (user_id) => {
         const res = await supabase
             .from('pipe_subscriptions')
-            .select('uploader_id')
+            .select('uploader_id, uuid, name, avatar')
             .eq('user_id', user_id)
 
         if (isValid(res.error)) {
@@ -13,10 +13,7 @@ export const user = {
             return { success: false, error: res.error }
         }
 
-        const uniqueUploaderIds = arr => [...new Set(arr.map(item => item.uploader_id))];
-        const list = uniqueUploaderIds(res.data);
-      
-        return { success: true, list: list }
+        return { success: true, list: res.data }
     }
 };
 
