8b22f90f8cccf113234e6376901391602a334204
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-08-19 01:46:18 +0530

    Update project

--- ios/PipePlusTests/PipePlusTests.m
+++ ios/PipePlusTests/PipePlusTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface pipeplusTests : XCTestCase
+@interface PipePlusTests : XCTestCase
 
 @end
 
-@implementation pipeplusTests
+@implementation PipePlusTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
