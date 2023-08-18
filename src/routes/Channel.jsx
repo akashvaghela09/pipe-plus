a1129446909f10137d6d4ed3bb970c3990f2a763
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-08-18 09:05:54 +0530

    Add new sections

--- src/routes/Channel.jsx
+++ src/routes/Channel.jsx
@@ -2,63 +2,188 @@ import { useEffect, useState, useRef } from "react";
 import { Link, useParams } from "react-router-dom";
 import { pipePlus } from "../apis";
 import { TiTick } from "react-icons/ti";
-import { formatNumbers, formatTime } from "../utils";
+import { formatNumbers, formatTime, isValid, waitFor } from "../utils";
 import { Spinner } from "../components";
+import { useDispatch, useSelector } from "react-redux";
+import { setPageLoading } from "../redux/app/actions";
+import { v4 as uuid } from 'uuid';
 
 export const Channel = () => {
+    const dispatch = useDispatch();
     const { channelId } = useParams();
     const [channel, setChannel] = useState(null);
     const [tab, setTab] = useState("videos");
-    const [nextpage, setNextpage] = useState(null);
     const [videoStreamList, setVideoStreamList] = useState([]);
+    const [featuredChannels, setFeaturedChannels] = useState([]);
+    const [nextpage, setNextpage] = useState(null);
+
     const videoSectionRef = useRef(null);
 
-    const fetchChannelData = async () => {
-        let res = await pipePlus.channel.getChannel(channelId);
+    const { pageLoading } = useSelector(state => state.app);
 
-        if (res.success === false) {
+    const fetchChannelData = async () => {
+        dispatch(setPageLoading(true));
+        let res = null;
+
+        if (tab === "videos") {
+            res = await pipePlus.channel.metadata(channelId);
+        } else {
+            let desiredTab = channel.tabs.find(item => {
+                return item.name === tab;
+            });
+
+            if (!isValid(desiredTab)) {
+                dispatch(setPageLoading(false));
+                return;
+            }
+
+            let newRes = await pipePlus.channel.tabsData(desiredTab.data);
+
+            if (!newRes.success) {
+                dispatch(setPageLoading(false));
+                return;
+            }
+
+            res = {
+                data: {
+                    relatedStreams: newRes.data.content,
+                    nextpage: newRes.data.nextpage
+                },
+                success: newRes.success
+            }
+        }
+
+        if (!isValid(res?.success)) {
+            dispatch(setPageLoading(false));
             return;
         }
 
-        setChannel(res.data);
-        setVideoStreamList(res.data.relatedStreams);
-        
-        let data = JSON.parse(res.data.nextpage);
+
+        if (channel === null) {
+            setChannel(res.data);
+        }
+
+        let list = res.data.relatedStreams.map(item => {
+            item.id = uuid();
+            return item;
+        });
+
+        if (tab === "channels") {
+            setFeaturedChannels([...featuredChannels, ...list]);
+        } else {
+            setVideoStreamList([...videoStreamList, ...list]);
+        }
+
+        if (res?.data?.nextpage === null) {
+            setNextpage(null);
+            dispatch(setPageLoading(false));
+            return;
+        }
+
+        let data = JSON.parse(res?.data?.nextpage);
+        if (!isValid(data)) {
+            dispatch(setPageLoading(false));
+            return;
+        }
+
         data.url = data.url.split("&prettyPrint=false")[0];
         let stringifiedData = JSON.stringify(data);
 
         setNextpage(stringifiedData);
+        dispatch(setPageLoading(false));
     }
 
-    const handleTabChange = (tab) => {
-        setTab(tab);
+    const handleTabChange = (value) => {
+        if (value === tab) {
+            return;
+        }
+
+        resetData();
+        setTab(value);
     }
 
     const loadMoreStreams = async () => {
-        console.log("loading more streams");
-        let res = await pipePlus.channel.getNextPage(channelId, nextpage);
+        let res;
 
-        if (res.success === false) {
+        if (tab === "videos") {
+            res = await pipePlus.channel.nextPage(channelId, nextpage);
+        } else {
+            let desiredTab = channel.tabs.find(item => {
+                return item.name === tab;
+            });
+
+            if (!isValid(desiredTab)) {
+                dispatch(setPageLoading(false));
+                return;
+            }
+
+            let newRes = await pipePlus.channel.tabsNextPage(desiredTab.data, nextpage);
+            console.log(newRes);
+            if (!newRes.success) {
+                dispatch(setPageLoading(false));
+                return;
+            }
+
+            res = {
+                data: {
+                    relatedStreams: newRes.data.content,
+                    nextpage: newRes.data.nextpage
+                },
+                success: newRes.success
+            }
+        }
+
+        if (!isValid(res?.success)) {
             return;
         } else if (res.data.relatedStreams.length === 0) {
             return;
         }
 
-        let data = JSON.parse(res.data.nextpage);
+        console.log("assigning uuid ...");
+        let list = res.data.relatedStreams.map(item => {
+            item.id = uuid();
+            return item;
+        });
+
+        if (tab === "channels") {
+            setFeaturedChannels([...featuredChannels, ...list]);
+        } else {
+            setVideoStreamList([...videoStreamList, ...list]);
+        }
+
+        if (res?.data?.nextpage === null) {
+            setNextpage(null);
+            dispatch(setPageLoading(false));
+            return;
+        }
+
+        let data = JSON.parse(res?.data?.nextpage);
+        if (!isValid(data)) {
+            dispatch(setPageLoading(false));
+            return;
+        }
+
         data.url = data.url.split("&prettyPrint=false")[0];
         let stringifiedData = JSON.stringify(data);
 
         setNextpage(stringifiedData);
-        setVideoStreamList([...videoStreamList, ...res.data.relatedStreams]);
+    }
+
+    const resetData = () => {
+        setTab("videos");
+        setNextpage(null);
+        setVideoStreamList([]);
+        setFeaturedChannels([]);
     }
 
     useEffect(() => {
         const handleScroll = async () => {
-            if(channel === null || nextpage === null){
+            const videoSectionElement = videoSectionRef?.current;
+
+            if (channel === null || nextpage === null || videoSectionElement === null) {
                 return;
             }
 
-            const videoSectionElement = videoSectionRef.current;
             let videoSectionHeight = videoSectionElement.clientHeight;
             let pageScroll = window.scrollY;
             let differece = videoSectionHeight - pageScroll;
@@ -76,11 +201,17 @@ export const Channel = () => {
     }, [nextpage]);
 
     useEffect(() => {
+        setChannel(null);
+        resetData();
         fetchChannelData();
     }, [channelId]);
 
+    useEffect(() => {
+        fetchChannelData();
+    }, [tab]);
+
     return (
-        channel === null ?
+        (pageLoading === true || channel === null) ?
             <div className="h-screen w-full flex justify-center items-center">
                 <Spinner />
             </div>
@@ -95,14 +226,15 @@ export const Channel = () => {
                         <TiTick className="text-xl bg-slate-100 text-black rounded-full" />
                     }
                 </span>
-                <p className="text-slate-100 text-opacity-50 text-sm">{formatNumbers(channel.subscriberCount)} subscribers</p>
+                <p className="text-slate-100 text-opacity-50 text-sm mt-2">{formatNumbers(channel.subscriberCount)} subscribers</p>
                 <p className="text-slate-100 text-opacity-50 line-clamp-2 m-2 text-sm text-center">{channel.description}</p>
 
                 <div className="mt-2 w-full">
                     <div className="flex">
-                        <p className={`cursor-pointer w-fit py-2 px-4 text-slate-100 active:bg-slate-100 active:bg-opacity-30 ${tab === "videos" && "border-b-4 border-slate-100 border-opacity-50"}`} onClick={() => handleTabChange("videos")}>Videos</p>
-                        <p className={`cursor-pointer w-fit py-2 px-4 text-slate-100 active:bg-slate-100 active:bg-opacity-30 ${tab === "shorts" && "border-b-4 border-slate-100 border-opacity-50"}`} onClick={() => handleTabChange("shorts")}>Shorts</p>
-                        <p className={`cursor-pointer w-fit py-2 px-4 text-slate-100 active:bg-slate-100 active:bg-opacity-30 ${tab === "livestreams" && "border-b-4 border-slate-100 border-opacity-50"}`} onClick={() => handleTabChange("livestreams")}>Livestreams</p>
+                        <p className={`cursor-pointer w-fit py-2 px-3 text-slate-100 active:bg-slate-100 active:bg-opacity-30 ${tab === "videos" && "border-b-4 border-slate-100 border-opacity-50"}`} onClick={() => handleTabChange("videos")}>Videos</p>
+                        <p className={`cursor-pointer w-fit py-2 px-3 text-slate-100 active:bg-slate-100 active:bg-opacity-30 ${tab === "shorts" && "border-b-4 border-slate-100 border-opacity-50"}`} onClick={() => handleTabChange("shorts")}>Shorts</p>
+                        <p className={`cursor-pointer w-fit py-2 px-3 text-slate-100 active:bg-slate-100 active:bg-opacity-30 ${tab === "livestreams" && "border-b-4 border-slate-100 border-opacity-50"}`} onClick={() => handleTabChange("livestreams")}>Livestreams</p>
+                        <p className={`cursor-pointer w-fit py-2 px-3 text-slate-100 active:bg-slate-100 active:bg-opacity-30 ${tab === "channels" && "border-b-4 border-slate-100 border-opacity-50"}`} onClick={() => handleTabChange("channels")}>Channels</p>
                     </div>
 
                     <div className="pt-5">
@@ -111,17 +243,83 @@ export const Channel = () => {
                             <div ref={videoSectionRef} className="flex flex-col gap-4">
                                 {
                                     videoStreamList.map((video, index) => {
-                                        return <Link to={video.url} key={video.url}>
-                                        <div className="flex w-full px-3">
-                                            <div className="relative w-[45%]">
-                                                <img className="rounded-xl w-full min-h-[88px]" alt="video-thumb" src={video.thumbnail} />
-                                                <p className="absolute bottom-1 right-1 text-slate-100 text-bold text-sm p-1 rounded-lg bg-black bg-opacity-80">{formatTime(video.duration)}</p>
+                                        return <Link to={video.url} key={video.id}>
+                                            <div className="flex w-full px-3">
+                                                <div className="relative w-[45%]">
+                                                    <img className="rounded-xl w-full min-h-[88px]" alt="video-thumb" src={video.thumbnail} />
+                                                    <p className="absolute bottom-1 right-1 text-slate-100 text-bold text-sm p-1 rounded-lg bg-black bg-opacity-80">{formatTime(video.duration)}</p>
+                                                </div>
+                                                <div className="w-[55%] px-2">
+                                                    <p className="text-slate-100 text-sm line-clamp-2 max-w-[200px] break-words">{video.title}</p>
+                                                    <p className="text-slate-100 text-opacity-50 text-xs">{formatNumbers(video.views)} views • {video.uploadedDate}</p>
+                                                </div>
                                             </div>
-                                            <div className="w-[55%] px-2">
-                                                <p className="text-slate-100 text-sm line-clamp-2 break-all">{video.title}</p>
-                                                <p className="text-slate-100 text-opacity-50 text-xs">{formatNumbers(video.views)} views • {video.uploadedDate}</p>
+                                        </Link>
+                                    })
+                                }
+                            </div>
+                        }
+
+                        {
+                            tab === "shorts" &&
+                            <div ref={videoSectionRef} className="flex flex-col gap-4">
+                                {
+                                    videoStreamList.map((video, index) => {
+                                        return <Link to={video.url} key={video.id}>
+                                            <div className="flex w-full px-3">
+                                                <div className="relative w-[45%]">
+                                                    <img className="rounded-xl w-full min-h-[88px]" alt="video-thumb" src={video.thumbnail} />
+                                                    <p className="absolute bottom-1 right-1 text-slate-100 text-bold text-sm p-1 rounded-lg bg-black bg-opacity-80">{formatTime(video.duration)}</p>
+                                                </div>
+                                                <div className="w-[55%] px-2">
+                                                    <p className="text-slate-100 text-sm line-clamp-2 max-w-[200px] break-words">{video.title}</p>
+                                                    <p className="text-slate-100 text-opacity-50 text-xs">{formatNumbers(video.views)} views • {video.uploadedDate}</p>
+                                                </div>
                                             </div>
-                                        </div>
+                                        </Link>
+                                    })
+                                }
+                            </div>
+                        }
+
+                        {
+                            tab === "livestreams" &&
+                            <div ref={videoSectionRef} className="flex flex-col gap-4">
+                                {
+                                    videoStreamList.map((video, index) => {
+                                        return <Link to={video.url} key={video.id}>
+                                            <div className="flex w-full px-3">
+                                                <div className="relative w-[45%]">
+                                                    <img className="rounded-xl w-full min-h-[88px]" alt="video-thumb" src={video.thumbnail} />
+                                                    <p className="absolute bottom-1 right-1 text-slate-100 text-bold text-sm p-1 rounded-lg bg-black bg-opacity-80">{formatTime(video.duration)}</p>
+                                                </div>
+                                                <div className="w-[55%] px-2">
+                                                    <p className="text-slate-100 text-sm line-clamp-2 max-w-[200px] break-words">{video.title}</p>
+                                                    <p className="text-slate-100 text-opacity-50 text-xs">{formatNumbers(video.views)} views • {video.uploadedDate}</p>
+                                                </div>
+                                            </div>
+                                        </Link>
+                                    })
+                                }
+                            </div>
+                        }
+
+                        {
+                            tab === "channels" &&
+                            <div ref={videoSectionRef} className="flex flex-col gap-8">
+                                {
+                                    featuredChannels.map((channel, index) => {
+                                        return <Link key={channel.id} to={channel.url}>
+                                            <div className="flex">
+                                                <div className="w-1/3 flex justify-center items-center">
+                                                    <img className="w-16 h-16 rounded-full" src={channel.thumbnail} alt="channel-avatar" />
+                                                </div>
+                                                <div className="grow">
+                                                    <p className="text-slate-100 line-clamp-1 text-sm">{channel.name}</p>
+                                                    <p className="text-slate-100 line-clamp-1 text-opacity-50 text-xs">{formatNumbers(channel.subscribers)}</p>
+                                                    <p className="text-slate-100 line-clamp-1 text-opacity-50 text-xs">{channel.videos} videos</p>
+                                                </div>
+                                            </div>
                                         </Link>
                                     })
                                 }
