40b644c174cbe774b86afb692610653670c4baa2
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-08-19 07:24:17 +0530

    Update routing

--- App.js
+++ App.js
@@ -1,13 +1,8 @@
-import { View, Text, ScrollView } from 'react-native';
+import AppNavigator from "./src/navigation/AppNavigator";
 
-const App = () => {
+function App() {
     return (
-        <ScrollView className="bg-[#0f0f0f]">
-            <View className="h-screen flex justify-center items-center">
-                <Text className="text-4xl text-blue-500">HELLO REACT NATIVE!</Text>
-            </View>
-        </ScrollView>
+            <AppNavigator />
     );
 }
-
 export default App;
