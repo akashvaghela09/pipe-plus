214cbaacabb3d465342de7e110f2b3d6309da275
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-08-19 19:05:12 +0530

    Update video card

--- src/components/ui/VideoCard.jsx
+++ src/components/ui/VideoCard.jsx
@@ -6,15 +6,15 @@ export const VideoCard = ({ video }) => {
         <View className="mb-4">
             <View className="relative">
                 <Image source={{ uri: video.thumbnail }} className="w-full aspect-video" />
-                <Text className="absolute bottom-0 right-0 px-1 m-1 rounded-md bg-black text-slate-100">{formatTime(video.duration)}</Text>
+                <Text className="absolute bottom-0 right-0 px-1 m-2 rounded-md bg-black text-slate-100">{formatTime(video.duration)}</Text>
             </View>
-            <View className="flex flex-row items-start">
-                <View className="p-2 py-[6px]">
+            <View className="flex flex-row items-start py-1">
+                <View className="p-2 py-2">
                     <Image source={{ uri: video.uploaderAvatar }} className="w-10 h-10 rounded-full" />
                 </View>
                 <View className="py-1 mx-1 w-full">
-                    <Text className="text-slate-100 line-clamp-2 break-all">{video.title}</Text>
-                    <Text className="text-slate-100 text-opacity-50 text-xs" style={{opacity: 0.5}}>{video.uploaderName} • {formatNumbers(video.views)} • {formatReadableDate(video.uploaded)}</Text>
+                    <Text className="text-slate-100 my-1" style={{fontWeight: "500"}}>{video.title}</Text>
+                    <Text className="text-slate-100 text-opacity-50 text-[11px]" style={{opacity: 0.5}}>{video.uploaderName} • {formatNumbers(video.views)} • {formatReadableDate(video.uploaded)}</Text>
                 </View>
             </View>
         </View>
