7500f393c309b068519280809455fe1c279dd442
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-08-19 08:31:07 +0530

    Add utils

---
+++ src/utils/index.js
@@ -0,0 +1,37 @@
+// Supabase Auth utils
+export { 
+    getUser, 
+    getSession, 
+    signInUser, 
+    signOutUser,
+    updateOnboardingStatus 
+} from './auth';
+
+// Fullscreen utils
+export { 
+    requestFullScreenEnter, 
+    requestFullScreenExit 
+} from './fullscreen';
+
+// Number formatting utils
+export { 
+    formatNumbers, 
+    formatTime,
+    formatDate,
+    formatIndianNumbering,
+    formatReadableDate 
+} from './numbers';
+
+// Localstorage utils
+export { 
+    loadData, 
+    saveData, 
+    clearData 
+} from './localstorage';
+
+// Helper utils
+export { 
+    waitFor, 
+    isValid,
+    shuffleArray 
+} from './helper';
