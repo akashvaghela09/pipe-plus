8255559abd26efcaefffa8b1d20b1ffdbc4a9a22
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-08-19 07:23:26 +0530

    Add base screens

---
+++ src/screens/LibraryScreen.jsx
@@ -0,0 +1,11 @@
+import { ScrollView, Text, View } from "react-native";
+
+export const LibraryScreen = () => {
+    return (
+        <ScrollView className="h-screen">
+            <View className="flex justify-center items-center h-screen">
+                <Text className="text-3xl font-bold">Library Screen</Text>
+            </View>
+        </ScrollView>
+    )
+};
---
+++ src/screens/index.js
@@ -0,0 +1,4 @@
+export { HomeScreen } from './HomeScreen';
+export { SubscriptionScreen } from './SubscriptionScreen';
+export { GroupScreen } from './GroupScreen';
+export { LibraryScreen } from './LibraryScreen';
