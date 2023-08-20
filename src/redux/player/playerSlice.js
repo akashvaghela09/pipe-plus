af2eb3a2e7ec0d3c5c56864797bcf3271ca909d0
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-08-20 20:07:39 +0530

    Add player slices

--- src/redux/player/playerSlice.js
+++ src/redux/player/playerSlice.js
@@ -1,23 +1,46 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    url: "",
+    streamUrl: "",
     isPlaying: false,
+    isFullScreen: false,
+    isVisible: false,
+    size: "normal",
+    settingsOpen: false,
 };
 
 const playerSlice = createSlice({
     name: 'player',
     initialState,
     reducers: {
-        setUrl: (state, action) => {
-            state.url = action.payload;
+        setStreamUrl: (state, action) => {
+            state.streamUrl = action.payload;
         },
         setIsPlaying: (state, action) => {
             state.isPlaying = action.payload;
+        },
+        setIsFullScreen: (state, action) => {
+            state.isFullScreen = action.payload;
+        },
+        setIsVisible: (state, action) => {
+            state.isVisible = action.payload;
+        },
+        setSize: (state, action) => {
+            state.size = action.payload;
+        },
+        setSettingsOpen: (state, action) => {
+            state.settingsOpen = action.payload;
         }
     },
 });
 
-export const { setUrl, setIsPlaying } = playerSlice.actions;
+export const { 
+    setStreamUrl, 
+    setIsPlaying, 
+    setIsFullScreen, 
+    setIsVisible ,
+    setSize,
+    setSettingsOpen
+} = playerSlice.actions;
 
 export default playerSlice.reducer;
